9a1b154da66f5d6857a6700a0d8f870532c5aab0
Brian Midei <brian.midei@gmail.com>
2019-11-20 20:21:24 -0500

    Added querying of data from netlify Function

--- ReportingTrends_website/src/components/layout.js
+++ ReportingTrends_website/src/components/layout.js
@@ -4,28 +4,15 @@
  *
  * See: https://www.gatsbyjs.org/docs/use-static-query/
  */
-
 import React from "react"
 import PropTypes from "prop-types"
-import { useStaticQuery, graphql } from "gatsby"
-
 import Header from "./header"
 import "./layout.css"
 
 const Layout = ({ children }) => {
-  const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
-        }
-      }
-    }
-  `)
-
   return (
-    <>
-      <Header siteTitle={data.site.siteMetadata.title} />
+    <div>
+      <Header siteTitle={"ReportingTrends"}/>
       <div
         style={{
           margin: `0 auto`,
@@ -41,7 +28,7 @@ const Layout = ({ children }) => {
           <a href="https://www.gatsbyjs.org">Gatsby</a>
         </footer>
       </div>
-    </>
+    </div>
   )
 }
 
--- ReportingTrends_website/src/pages/index.js
+++ ReportingTrends_website/src/pages/index.js
@@ -3,21 +3,16 @@ import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import Image from "../components/image"
-import SEO from "../components/seo"
-// import BarChart from "../components/BarChart"
 import EconomicDataTSNE from "../components/EconomicDataTSNE"
-
-
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="Reporting Trends" />
+    {/*<SEO title="Reporting Trends" />*/}
     <h1>Analyzing Reporting Trends in the New York Times</h1>
-    <p>Now go build something great, bro.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
-    <EconomicDataTSNE />
+    <EconomicDataTSNE width={600} height={400}/>
     {/*<BarChart data={[5,10,1,3]} size={[500,500]} />*/}
     <Link to="/page-2/">Go to page 2</Link>
   </Layout>
