0e9f5ad7df4ab391e1e493ec55ec7e6359dc9eb9
Brian Midei <brian.midei@gmail.com>
2019-12-03 10:04:32 -0500

    Added LIWC line chart

--- ReportingTrends_website/src/components/LIWCLineChart.js
+++ ReportingTrends_website/src/components/LIWCLineChart.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import * as d3 from 'd3'
+import { ResponsiveLine } from '@nivo/line'
 
 
 class LIWCLineChart extends Component {
@@ -7,14 +7,15 @@ class LIWCLineChart extends Component {
   // const [count, setCount] = useState(0);
   // set the dimensions and margins of the graph
   state = {
-    data: []
+    data: [],
+    chartData: []
   };
 
   queryData() {
     // Toggle these two lines between local dev and deployment
-    // return fetch('https://eloquent-blackwell-3fb9dd.netlify.com/.netlify/functions/EconomicData', {
-    return fetch('/.netlify/functions/EconomicData', {
-      headers: { accept: "Accept: application/json" },
+    // return fetch('https://eloquent-blackwell-3fb9dd.netlify.com/.netlify/functions/LIWCData', {
+      return fetch('/.netlify/functions/LIWCData', {
+      headers: {accept: "Accept: application/json"},
       method: 'POST',
     }).then(response => {
       return response.json()
@@ -25,131 +26,78 @@ class LIWCLineChart extends Component {
     console.log('fetching data');
     this.queryData().then((response) => {
       // Store queried data in state
-      this.setState({data: response});
+      this.formatData(response)
     }).catch((error) => {
       console.log('API error', error)
     });
-    this.createChart()
+
   };
 
-  componentDidUpdate() {
-    this.createChart()
-  };
-
-  createChart() {
+  formatData(data) {
     // Sort data by year
-    let data = this.state.data;
-    data.sort((a, b) => (a.year > b.year) ? -1 : 1);
-
-    let margin = {top: 10, right: 50, bottom: 40, left: 60},
-      width = this.props.width - margin.left - margin.right,
-      height = this.props.height - margin.top - margin.bottom;
-
-    const node = this.node;
-
-    let line_svg = d3.select(node)
-      .append("svg")
-      .attr("width", width + margin.left + margin.right)
-      .attr("height", height + margin.top + margin.bottom)
-      .append("g")
-      .attr("transform",
-        "translate(" + margin.left + "," + margin.top + ")");
-
-    // let div = d3.select("body").append("div")
-    //   .attr("class", "tooltip")
-    //   .style("opacity", 0);
-
-    let x = d3.scaleLinear()
-      .domain([1941, 2010])
-      .range([0, width]);
-    line_svg.append("g")
-      .attr("transform", "translate(0," + height + ")")
-      .call(d3.axisBottom(x).tickFormat(d3.format("d")));
-
-    // Add Y axis
-    let y = d3.scaleLinear()
-      .domain([d3.min(data, function (d) {
-        return d.Inflation;
-      }), d3.max(data, function (d) {
-        return d.Inflation;
-      })])
-      .range([height, 0]);
-    line_svg.append("g")
-      .call(d3.axisLeft(y));
-
-    let y2 = d3.scaleLinear()
-      .domain([d3.min(data, function (d) {
-        return d.unemployed_percent;
-      }), d3.max(data, function (d) {
-        return d.unemployed_percent;
-      })])
-      .range([height, 0]);
-    line_svg.append("g")
-      .attr("transform", "translate( " + width + ", 0 )")
-      .call(d3.axisRight(y2));
-
-    // Add the line
-    line_svg.append("path")
-      .datum(data)
-      .attr("fill", "none")
-      .attr("stroke", "darkOrange")
-      .attr("stroke-width", 1.5)
-      .attr("d", d3.line()
-        .x(function (d) {
-          return x(d.year)
-        })
-        .y(function (d) {
-          return y(d.Inflation)
-        })
-      )
-
-    // Add the line
-    line_svg.append("path")
-      .datum(data)
-      .attr("fill", "none")
-      .attr("stroke", "steelblue")
-      .attr("stroke-width", 1.5)
-      .attr("d", d3.line()
-        .x(function (d) {
-          return x(d.year)
-        })
-        .y(function (d) {
-          return y2(d.unemployed_percent)
-        })
-      )
-
-    line_svg.append("text")
-      .attr("transform",
-        "translate(" + (width / 2) + " ," +
-        (height + margin.top + 25) + ")")
-      .style("text-anchor", "middle")
-      .text("Year");
-
-    line_svg.append("text")
-      .attr("transform", "rotate(-90)")
-      .attr("y", 0 - margin.left)
-      .attr("x", 0 - (height / 2))
-      .attr("dy", "1em")
-      .style("text-anchor", "middle")
-      .style('fill', 'darkOrange')
-      .text("Inflation");
-
-    line_svg.append("text")
-      .attr("transform", "rotate(-90)")
-      .attr("y", 20 + width)
-      .attr("x", 0 - (height / 2))
-      .attr("dy", "1em")
-      .style("text-anchor", "middle")
-      .style('fill', 'steelblue')
-      .text("Unemployed percent");
+    data.sort((a, b) => (a.year < b.year) ? -1 : 1);
+    let posemoArr = data.map(function (elem) {
+      return {
+        'x': elem['year'],
+        'y': elem['posemo']
+      }
+    });
+    let posemoData = {
+      id: 'Positive Emotion',
+      data: posemoArr
+    };
+    let negemoArr = data.map(function (elem) {
+      return {
+        'x': elem['year'],
+        'y': elem['negemo']
+      }
+    });
+    let negemoData = {
+      id: 'Negative Emotion',
+      data: negemoArr
+    };
+    this.setState({chartData: [posemoData, negemoData]});
   }
 
   render() {
     return (
-      <svg ref={node => this.node = node}
-           width={1000} height={500}>
-      </svg>
-    );
+    <div style={{ height: 500 }}>
+      <ResponsiveLine
+      data={this.state.chartData}
+      margin={{ top: 50, right: 110, bottom: 50, left: 60 }}
+      xScale={{ type: 'linear', min: 'auto', max: 'auto' }}
+      yScale={{ type: 'linear', stacked: true, min: 'auto', max: 'auto' }}
+      axisTop={null}
+      axisRight={null}
+      axisBottom={{
+        orient: 'bottom',
+        tickSize: 5,
+        tickPadding: 5,
+        tickRotation: 0,
+        legend: 'Date',
+        legendOffset: 36,
+        legendPosition: 'middle'
+      }}
+      axisLeft={{
+        orient: 'left',
+        tickSize: 5,
+        tickPadding: 5,
+        tickRotation: 0,
+        legend: 'Emotion',
+        legendOffset: -40,
+        legendPosition: 'middle'
+      }}
+      colors={{ scheme: 'nivo' }}
+      pointSize={10}
+      pointColor={{ theme: 'background' }}
+      pointBorderWidth={2}
+      pointBorderColor={{ from: 'serieColor' }}
+      // pointLabel=y
+      pointLabelYOffset={-12}
+      useMesh={true}
+      />
+    </div>
+    )
   }
 }
 export default LIWCLineChart;
