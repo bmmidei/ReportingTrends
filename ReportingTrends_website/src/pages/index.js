da63248c8740acbeeb8791027db2ff8ed1ba1254
Brian Midei <brian.midei@gmail.com>
2019-12-09 20:45:17 -0500

    Added data query to upper-level component

--- ReportingTrends_website/src/pages/index.js
+++ ReportingTrends_website/src/pages/index.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, {Component} from "react"
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
@@ -6,19 +6,60 @@ import Layout from "../components/layout"
 import EconomicDataTSNE from "../components/EconomicDataTSNE"
 import WordCountsLineChart from "../components/WordCountsLineChart";
 import LIWCLineChart from "../components/LIWCLineChart";
+import TopicLineChart from "../components/TopicLineChart";
 
-const IndexPage = () => (
-  <Layout>
-    {/*<SEO title="Reporting Trends" />*/}
-    <h1>Analyzing Reporting Trends in the New York Times</h1>
-    {/*<div style={{maxWidth: `300px`, marginBottom: `1.45rem`}}>*/}
-    {/*  <Image/>*/}
-    {/*</div>*/}
-    <EconomicDataTSNE width={600} height={400}/>
-    <WordCountsLineChart width={600} height={400}/>
-    <LIWCLineChart />
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
-);
+class IndexPage extends Component {
 
-export default IndexPage
+  state = {
+    EconomicData: [],
+    LIWCData: [],
+    TopicData: [],
+  };
+
+  queryData = (source) => {
+    // Toggle these two lines between local dev and deployment
+    return fetch('https://eloquent-blackwell-3fb9dd.netlify.com/.netlify/functions/' + source, {
+      // return fetch('/.netlify/functions/' + source, {
+      headers: { accept: "Accept: application/json" },
+      method: 'POST',
+    }).then(response => {
+      return response.json()
+    })
+  };
+
+  componentDidMount() {
+    let dataSources = ['EconomicData', 'LIWCData', 'TopicData'];
+    console.log('fetching data');
+    dataSources.forEach(source => {
+      this.queryData(source).then((response) => {
+        // Store queried data in state
+        this.setState({[source]: response});
+      }).catch((error) => {
+        console.log('API error', error)
+      });
+    });
+  };
+
+  render() {
+    const { EconomicData } = this.state;
+    const { LIWCData } = this.state;
+    const { TopicData } = this.state;
+    return (
+      <Layout>
+        {/*<SEO title="Reporting Trends" />*/}
+        <h1>Analyzing Reporting Trends in the New York Times</h1>
+        {/*<div style={{maxWidth: `300px`, marginBottom: `1.45rem`}}>*/}
+        {/*  <Image/>*/}
+        {/*</div>*/}
+        {EconomicData && <EconomicDataTSNE data={EconomicData} width={600} height={400}/>}
+        {EconomicData && <WordCountsLineChart data={EconomicData} width={600} height={400}/>}
+        {LIWCData && <LIWCLineChart data={LIWCData}/>}
+        {TopicData && <TopicLineChart height={500} data={TopicData}/>}
+        <Link to="/page-2/">Go to page 2</Link>
+      </Layout>
+    )
+  }
+}
+
+
+export default IndexPage;
