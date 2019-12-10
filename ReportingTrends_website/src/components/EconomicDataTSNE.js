21f19ea441198ecbc4844d1d2d6974d85e701379
Brian Midei <brian.midei@gmail.com>
2019-12-10 15:12:01 -0500

    Added nivo scatterplot

--- ReportingTrends_website/src/components/EconomicDataTSNE.js
+++ ReportingTrends_website/src/components/EconomicDataTSNE.js
@@ -1,129 +1,94 @@
-import React, {Component} from 'react';
-import * as d3 from 'd3'
+import React, {Component, Fragment} from 'react';
+import { ResponsiveScatterPlot } from '@nivo/scatterplot'
+import {ResponsiveLine} from "@nivo/line";
 
 
 class EconomicDataTSNE extends Component {
   // Initialize with no data
 
-  componentDidMount() {
-    this.createChart()
+  formatRenderedData = () => {
+    let clusters = [...new Set(this.props.data.map(({cluster}) => (cluster)))].sort();
+    return clusters.map((cluster) => {
+      let clusterData = this.props.data.filter(elem => elem['cluster'] === cluster);
+      return {id: String(cluster), data: clusterData}
+    });
   };
-
-  componentDidUpdate() {
-    this.createChart()
-  };
-
-  createChart() {
-    let data = this.props.data;
-    let left = d3.min(data, function (d) { return d.x });
-    let right = d3.max(data, function (d) { return d.x });
-    let bottom = d3.min(data, function (d) { return d.y });
-    let top = d3.max(data, function (d) { return d.y });
-    let buffer = 1;
-    const node = this.node;
-    // set the dimensions and margins of the graph
-    let margin = {top: 10, right: 50, bottom: 40, left: 60},
-      width = this.props.width - margin.left - margin.right,
-      height = this.props.height - margin.top - margin.bottom;
-
-    let svg = d3.select(node)
-      .append("svg")
-      .attr("width", width + margin.left + margin.right)
-      .attr("height", height + margin.top + margin.bottom)
-      .append("g")
-      .attr("transform",
-        "translate(" + margin.left + "," + margin.top + ")");
-
-    let cValue = function(d) { return d.cluster;},
-      color = d3.scaleOrdinal(d3.schemeCategory10);
-
-    // Add X axis
-    let x = d3.scaleLinear()
-      .domain([left-buffer, right+buffer])
-      .range([ 0, width ]);
-    svg.append("g")
-      .attr("transform", "translate(0," + height + ")")
-      .call(d3.axisBottom(x))
-      .append("text")
-      .text("Protein (g)");
-
-    // Add Y axis
-    let y = d3.scaleLinear()
-      .domain([bottom-buffer, top+buffer])
-      .range([ height, 0]);
-    svg.append("g")
-      .call(d3.axisLeft(y));
-
-    let div = d3.select('body').append("div")
-      .attr("class", "tooltip")
-      .style("opacity", 0)
-      .style("position", "absolute")
-      .style("text-align", "center")
-      .style("width", "120px")
-      .style("height", "58px")
-      .style("padding", "2px")
-      .style("font", "12px sans-serif")
-      .style("background",  "lightsteelblue")
-      .style("border", "0px")
-      .style("border-radius", "8px")
-      .style("pointer-events", "none");
-
-    // Add dot")
-    svg.append('g')
-      .selectAll("dot")
-      .data(data)
-      .enter()
-      .append("circle")
-      .attr("cx", function (d) { return x(d.x); } )
-      .attr("cy", function (d) { return y(d.y); } )
-      .attr("r", 5.5)
-      .style("fill", function(d) { return color(cValue(d));})
-      .on("mouseover", function(d) {
-        div.transition()
-          .duration(200)
-          .style("opacity", .9);
-        div.html("Year: " + d.year + "<br/>Inflation: " + d.Inflation.toFixed(2) + "<br/>Employed: " + d.employed_percent + "%<br/>Unemployed: " + d.unemployed_percent+ "%")
-          .style("left", (d3.event.pageX) + "px")
-          .style("top", (d3.event.pageY - 28) + "px");
-      })
-      .on("mouseout", function(d) {
-        div.transition()
-          .duration(200)
-          .style("opacity", 0);
-      });
-
-    svg.append("text")
-      .attr("transform",
-        "translate(" + (width/2) + " ," +
-        (height + margin.top + 25) + ")")
-      .style("text-anchor", "middle")
-      .text("TSNE Dimension 1");
-
-    svg.append("text")
-      .attr("transform", "rotate(-90)")
-      .attr("y", 0 - margin.left)
-      .attr("x",0 - (height / 2))
-      .attr("dy", "1em")
-      .style("text-anchor", "middle")
-      .text("TSNE Dimension 2");
-
-    svg.append('g')
-      .selectAll("text")
-      .data(data)
-      .enter()
-      .append('text')
-      .style("font", "10px sans-serif")
-      .attr("x", function (d) { return x(d.x); } )
-      .attr("y", function (d) { return y(d.y); } )
-      .text(function(d){return d.year})
-    }
-
 
   render() {
+    let renderedData = this.formatRenderedData();
+    console.log(renderedData);
     return (
-      <svg ref={node => this.node = node}
-           width={this.props.width} height={this.props.height}>
-      </svg>
+      <Fragment>
+        <div style={{ height: this.props.height }}>
+          <ResponsiveScatterPlot
+            data={renderedData}
+            margin={{ top: 60, right: 140, bottom: 70, left: 90 }}
+            xScale={{ type: 'linear', min: 'auto', max: 'auto' }}
+            // xFormat={function(e){return e+" kg"}}
+            yScale={{ type: 'linear', min: 'auto', max: 'auto' }}
+            // yFormat={function(e){return e+" cm"}}
+            blendMode="multiply"
+            useMesh={false}
+            enableGridX={false}
+            enableGridY={false}
+            gridXValues={null}
+            gridYValues={null}
+            axisTop={null}
+            axisRight={null}
+            axisBottom={{
+              orient: 'bottom',
+              tickSize: 5,
+              tickPadding: 5,
+              tickRotation: 0,
+              legend: 'TSNE Dimension 1',
+              legendPosition: 'middle',
+              legendOffset: 46
+            }}
+            axisLeft={{
+              orient: 'left',
+              tickSize: 5,
+              tickPadding: 5,
+              tickRotation: 0,
+              legend: 'TSNE Dimension 2',
+              legendPosition: 'middle',
+              legendOffset: -60
+            }}
+            tooltip={(elem) => {
+              let data = elem['node']['data'];
+              return (
+                <div>
+                  <span>Year: {data['year']} </span>
+                  <span>Inflation: {data['Inflation'].toFixed(2)} </span>
+                  <span>Unemployment: {data['unemployed_percent'].toFixed(2)} </span>
+                </div>
+              )}
+            }
+            legends={[
+              {
+                anchor: 'bottom-right',
+                direction: 'column',
+                justify: false,
+                translateX: 130,
+                translateY: 0,
+                itemWidth: 100,
+                itemHeight: 12,
+                itemsSpacing: 5,
+                itemDirection: 'left-to-right',
+                symbolSize: 12,
+                symbolShape: 'circle',
+                effects: [
+                  {
+                    on: 'hover',
+                    style: {
+                      itemOpacity: 1
+                    }
+                  }
+                ]
+              }
+            ]}
+          />
+        </div>
+      </Fragment>
     );
   }
 }
